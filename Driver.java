1e688ad8bceefa97834863ca877d9722074a8e68
Joe Kerrigan <joek1301@gmail.com>
2019-10-21 21:07:54 -0400

    Add ArrayList of volunteers representing a Driver's riders

--- Driver.java
+++ Driver.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
 
 public class Driver extends Volunteer {
 	private int seatsInCar;
+	private ArrayList<Volunteer> riders;
 
 	public Driver(String firstName, String lastName, String email, String phoneNo, String preferredSchool, boolean isCurry, boolean isSpanishSpeaking, int seatsInCar) {
 		super(firstName, lastName, email, phoneNo, preferredSchool, isCurry, isSpanishSpeaking);
 		this.seatsInCar = seatsInCar;
+		this.riders = new ArrayList<Volunteer>();
 	}
 	public int getSeats() {
 		return seatsInCar;
+	}
+	public void addRider(Volunteer v) {
+		this.riders.add(v);
+	}
+	public ArrayList<Volunteer> getRiders() {
+	    return this.riders;
 	}
 
 }
