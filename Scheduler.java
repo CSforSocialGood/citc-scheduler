ad8652cb6d2ef47c3acd63500438536894fb3f7e
Joe Kerrigan <joek1301@gmail.com>
2019-11-03 14:44:57 -0500

    Improve scheduling logic to consider multiple availabilities

--- Scheduler.java
+++ Scheduler.java
@@ -19,18 +19,37 @@ public class Scheduler {
      */
     public void assignDrivers() {
         ArrayList<Driver> driversRemaining = (ArrayList) drivers.clone(); // clone the list of drivers so we don't mutate the original source
+        ArrayList<Driver> driversAssigned = new ArrayList<>();
 
         int currentTeacher = 0;
+        Teacher teacher = teachers.get(currentTeacher % teachers.size());
         while(driversRemaining.size() > 0) {
             for(Driver d : driversRemaining) {
-                TimeBlockAndDay overlap;
-                if((overlap = d.getAvailability().findOverlap(teachers.get(currentTeacher % teachers.size()).getAvailability())) != null) {
-                    currentTeacher += 1;
-                    teachers.get(currentTeacher % teachers.size()).assignVolunteer(d);
-                    d.setAssignedTime(overlap.getDay(), overlap.getTimeBlock());
-                    driversRemaining.remove(d);
-                    break;
+                boolean driverAssigned = false;
+                ArrayList<TimeBlockAndDay> overlaps;
+                if((overlaps = d.getAvailability().findOverlaps(teachers.get(currentTeacher % teachers.size()).getAvailability())).size() != 0) {
+                    for(TimeBlockAndDay overlap : overlaps) {
+                        boolean conflictFound = false;
+                        for(Driver assigned : driversAssigned) {
+                            if(assigned.getTeacher().getSchool() == teacher.getSchool() && assigned.getAssignedDayOfWeek() == overlap.getDay() && assigned.getAssignedTime().overlapWith(overlap.getTimeBlock()) != null) {
+                                // these drivers are assigned on the same day
+                                conflictFound = true;
+                                break;
+                            }
+                        }
+                        if(!conflictFound) {
+                            // we can safely assign this driver to this time
+                            currentTeacher += 1;
+                            teacher.assignVolunteer(d);
+                            d.setAssignedTime(overlap.getDay(), overlap.getTimeBlock());
+                            driversRemaining.remove(d);
+                            driversAssigned.add(d);
+                            driverAssigned = true;
+                            break;
+                        }
+                    }
                 }
+                if(driverAssigned) break;
             }
         }
     }
